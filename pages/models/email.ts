dee7652d2a547caaba776fccbd276c24975f04fb
Joseph Martinsen <joseph@martinsen.com>
2020-11-08 19:47:55 -0600

    add tailwindcss

---
+++ pages/models/email.ts
@@ -0,0 +1,22 @@
+class EmailController {
+  private static instance: EmailController;
+
+  private constructor() {
+    // TODO: setup connection to mail service
+  }
+
+  public static getInstance(): EmailController {
+    if (!EmailController.instance) {
+      EmailController.instance = new EmailController();
+    }
+
+    return EmailController.instance;
+  }
+
+  public sendEmail() {
+    // TODO: actually send the email(s)?
+    return;
+  }
+}
+
+export default EmailController;
