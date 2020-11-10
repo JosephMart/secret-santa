6abe7b10a875d8657249656ed9da7628fa6f2c60
Joseph Martinsen <joseph@martinsen.com>
2020-11-10 00:10:08 -0600

    commit new files

---
+++ pages/components/SantaForm.tsx
@@ -0,0 +1,48 @@
+import styles from "../../styles/SantaForm.module.css";
+import Select from "react-select";
+
+const options = [
+  { value: "chocolate", label: "Chocolate" },
+  { value: "strawberry", label: "Strawberry" },
+  { value: "vanilla", label: "Vanilla" },
+];
+
+export default function SantaForm() {
+  return (
+    <div className={styles.SantaForm}>
+      {/* Header row */}
+      <div className={styles.row}>
+        <div className={styles.col}>
+          <h2 className={styles.exclude}>Name</h2>
+        </div>
+        <div className={styles.col}>
+          <h2 className={styles.exclude}>Email</h2>
+        </div>
+        <div className={styles.col}>
+          <h2 className={styles.exclude}>Does not match with</h2>
+        </div>
+      </div>
+      {/* Input rows */}
+      <div className={styles.row}>
+        <div className={styles.col}>
+          <input className={styles.inputText} type="text" />
+        </div>
+        <div className={styles.col}>
+          <input className={styles.inputText} type="text" />
+        </div>
+        <div className={styles.col + " " + styles.select}>
+          {/* <Select id="31" options={options} isMulti /> */}
+          <select name="pets" id="pet-select" multiple>
+            <option value="">--Please choose an option--</option>
+            <option value="dog">Dog</option>
+            <option value="cat">Cat</option>
+            <option value="hamster">Hamster</option>
+            <option value="parrot">Parrot</option>
+            <option value="spider">Spider</option>
+            <option value="goldfish">Goldfish</option>
+          </select>
+        </div>
+      </div>
+    </div>
+  );
+}
